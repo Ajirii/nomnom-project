36b6239dd2c19ddcbea56899a3f1c471a0a3a39b
Lam Pham <lampham28864@gmail.com>
2025-04-03 05:38:54 -0400

    testing images for navbar

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -2,14 +2,11 @@ import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 //import navbar images
-import carrot_pixel from "../../assets/pixel_images/carrot_pixel.png";
-import soy_pixel from "../../assets/pixel_images/soy_pixel.png";
-import corn_pixel from "../../assets/pixel_images/corn_pixel.png";
-import cookie_pixel from "../../assets/pixel_images/cookie_pixel.png";
-import noodle_pixel from "../../assets/pixel_images/noodle_pixel.png";
-import orange_pixel from "../../assets/pixel_images/orange_pixel.png";
-import salad_pixel from "../../assets/pixel_images/salad_pixel.png";
-import onion_pixel from "../../assets/pixel_images/onion_pixel.png";
+import cheesecake from "../../assets/pixel_images/23_cheesecake_dish.png";
+import mirror from "../../assets/pixel_images/item_mirror.png";
+import book from "../../assets/pixel_images/item_book_red_lines.png";
+import flower from "../../assets/pixel_images/item_flower_daisy.png";
+import basket from "../../assets/pixel_images/basket_yellow.png";
 
 interface NavbarProps {
   onLinkClick: (component: string) => void;
@@ -21,30 +18,30 @@ const Navbar = ({ onLinkClick }: NavbarProps) => {
   return (
     <nav className="navbar">
       <div className="navbar_heading">
-        <img src={carrot_pixel} className="navbar_heading_emote" />
+        <img src={basket} className="navbar_heading_emote" />
         <h1 className="navbar_title">NomNom</h1>
       </div>
       <ul className="navbar_links">
         <li className="navbar_font">
-          <img src={soy_pixel} className="navbar_emotes" />
+          <img src={flower} className="navbar_emotes" />
           <a href="#home" onClick={() => onLinkClick("home")}>
             Home
           </a>
         </li>
         <li className="navbar_font">
-          <img src={corn_pixel} className="navbar_emotes" />
+          <img src={mirror} className="navbar_emotes" />
           <a href="#companion" onClick={() => onLinkClick("companion")}>
             Companion
           </a>
         </li>
         <li className="navbar_font">
-          <img src={orange_pixel} className="navbar_emotes" />
+          <img src={book} className="navbar_emotes" />
           <a href="#quests" onClick={() => onLinkClick("quests")}>
             Quests
           </a>
         </li>
         <li className="navbar_font">
-          <img src={cookie_pixel} className="navbar_emotes" />
+          <img src={cheesecake} className="navbar_emotes" />
           <a href="#recipes" onClick={() => onLinkClick("recipes")}>
             Recipes
           </a>
