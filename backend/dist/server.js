f30b785350384dfc44d521b3d90c80c3df1ae386
Lynn Pham <lynnpham28714@gmail.com>
2025-04-09 07:23:25 -0400

    updated scripts and tsconfig

---
+++ backend/dist/server.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+/* ROUTE IMPORT */
+// import recipeRoutes from "./routes/recipes.routes";
+// import googleRoutes from "./routes/google.routes";
+/* CONFIGURATIONS */
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+/* ROUTES */
+// app.use("/api/recipes", recipeRoutes);
+// app.use("/api/google", googleRoutes);
+/* SERVER */
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server running on port ${port}`);
+});
