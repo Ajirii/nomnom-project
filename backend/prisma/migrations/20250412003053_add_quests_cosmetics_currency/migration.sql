e2e01cc1434f93e12c04608289b124333db4efab
Lynn Pham <lynnpham28714@gmail.com>
2025-04-11 21:18:00 -0400

    added seeding, updated database table, and updated recipes backend

--- backend/prisma/migrations/20250412003053_add_quests_cosmetics_currency/migration.sql
+++ backend/prisma/migrations/20250412003053_add_quests_cosmetics_currency/migration.sql
@@ -12,9 +12,6 @@
   - The required column `userId` was added to the `User` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
 
 */
--- CreateEnum
-CREATE TYPE "Status" AS ENUM ('available', 'accepted', 'completed');
-
 -- AlterTable
 ALTER TABLE "Recipe" DROP CONSTRAINT "Recipe_pkey",
 DROP COLUMN "id",
@@ -26,6 +23,7 @@ ADD CONSTRAINT "Recipe_pkey" PRIMARY KEY ("recipeId");
 -- AlterTable
 ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
 DROP COLUMN "id",
+ADD COLUMN     "currency" INTEGER NOT NULL DEFAULT 0,
 ADD COLUMN     "googleId" TEXT NOT NULL,
 ADD COLUMN     "userId" TEXT NOT NULL,
 ADD CONSTRAINT "User_pkey" PRIMARY KEY ("userId");
@@ -35,34 +33,21 @@ CREATE TABLE "Quest" (
     "questId" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    "reward" INTEGER NOT NULL,
-    "status" "Status" NOT NULL,
-    "isCompleted" BOOLEAN NOT NULL DEFAULT false,
-    "userId" TEXT NOT NULL,
+    "rewardCurrency" INTEGER NOT NULL DEFAULT 0,
+    "rewardHunger" INTEGER NOT NULL DEFAULT 0,
+    "userUserId" TEXT,
 
     CONSTRAINT "Quest_pkey" PRIMARY KEY ("questId")
 );
 
 -- CreateTable
-CREATE TABLE "Achievement" (
-    "achievementId" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "iconUrl" TEXT,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "cosmeticId" TEXT,
-
-    CONSTRAINT "Achievement_pkey" PRIMARY KEY ("achievementId")
-);
-
--- CreateTable
-CREATE TABLE "UserAchievement" (
-    "userAid" TEXT NOT NULL,
+CREATE TABLE "UserQuest" (
+    "userQuestId" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
-    "achievementId" TEXT NOT NULL,
-    "unlockedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "questId" TEXT NOT NULL,
+    "completedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "UserAchievement_pkey" PRIMARY KEY ("userAid")
+    CONSTRAINT "UserQuest_pkey" PRIMARY KEY ("userQuestId")
 );
 
 -- CreateTable
@@ -71,6 +56,7 @@ CREATE TABLE "Cosmetic" (
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "iconUrl" TEXT,
+    "price" INTEGER NOT NULL,
 
     CONSTRAINT "Cosmetic_pkey" PRIMARY KEY ("cosmeticId")
 );
@@ -85,28 +71,19 @@ CREATE TABLE "UserCosmetic" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Achievement_cosmeticId_key" ON "Achievement"("cosmeticId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "UserAchievement_userId_achievementId_key" ON "UserAchievement"("userId", "achievementId");
-
--- CreateIndex
 CREATE UNIQUE INDEX "UserCosmetic_userId_cosmeticId_key" ON "UserCosmetic"("userId", "cosmeticId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_googleId_key" ON "User"("googleId");
 
 -- AddForeignKey
-ALTER TABLE "Quest" ADD CONSTRAINT "Quest_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Quest" ADD CONSTRAINT "Quest_userUserId_fkey" FOREIGN KEY ("userUserId") REFERENCES "User"("userId") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Achievement" ADD CONSTRAINT "Achievement_cosmeticId_fkey" FOREIGN KEY ("cosmeticId") REFERENCES "Cosmetic"("cosmeticId") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "UserQuest" ADD CONSTRAINT "UserQuest_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "UserAchievement" ADD CONSTRAINT "UserAchievement_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "UserAchievement" ADD CONSTRAINT "UserAchievement_achievementId_fkey" FOREIGN KEY ("achievementId") REFERENCES "Achievement"("achievementId") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "UserQuest" ADD CONSTRAINT "UserQuest_questId_fkey" FOREIGN KEY ("questId") REFERENCES "Quest"("questId") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "UserCosmetic" ADD CONSTRAINT "UserCosmetic_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
--- backend/src/recipes/recipes.service.ts
+++ backend/src/recipes/recipes.service.ts
@@ -1,22 +1,25 @@
-import OpenAI from 'openai'
-const { PrismaClient } = require('@prisma/client');
+import OpenAI from "openai";
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const openai = new OpenAI({apiKey:process.env.OPENAI_API_KEY});
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-
-export const getRecipeByIngredients = async (ingredients: string, cuisine: string, strict: string) => {
-    let strictMode: boolean = false;
-    if (strict === "true"){
-        strictMode = true
-    }
-    try {
-        const response = await openai.chat.completions.create({
-            model: "gpt-4o-mini",
-            messages: [
-                {
-                    role: "system",
-                    content: `
+export const getRecipeByIngredients = async (
+  ingredients: string,
+  cuisine: string,
+  strict: string
+) => {
+  let strictMode: boolean = false;
+  if (strict === "true") {
+    strictMode = true;
+  }
+  try {
+    const response = await openai.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
+        {
+          role: "system",
+          content: `
                 You are a professional and imaginative AI chef. Your goal is to create unique, delicious, and creative recipes using the ingredients provided by the user. You specialize in global cuisines, creative flavor combinations, and naming dishes in a way that is fun, enticing, or culturally inspired.
                 
                 Always respond ONLY in JSON format, structured as:
@@ -30,103 +33,136 @@ export const getRecipeByIngredients = async (ingredients: string, cuisine: strin
                 }
                 
                 Focus on originality, flavor, and a playful yet professional tone.
-                ${strictMode ? `IMPORTANT RULE: You MUST NOT use any ingredients that are not listed by the user. 
+                ${
+                  strictMode
+                    ? `IMPORTANT RULE: You MUST NOT use any ingredients that are not listed by the user. 
                                 Do not assume pantry items, spices, or common ingredients unless they are explicitly mentioned. 
                                 Do not use salt, oil, water, or any ingredient unless it appears in the user's list.
-                                ` : ""}
-                `
-                },
-                {
-                    role: "user",
-                    content: `Using the ingredients: ${ingredients}, generate a ${cuisine}-inspired recipe. Be imaginative with the flavor profile and give the dish a fun or culturally relevant name. Make sure it includes creative but approachable instructions and fits within the JSON structure.`
+                                `
+                    : ""
                 }
-            ],
-            response_format: { type: "json_object" },
-            temperature: 0.7,
-            max_tokens: 700,
-        });
+                `,
+        },
+        {
+          role: "user",
+          content: `Using the ingredients: ${ingredients}, generate a ${cuisine}-inspired recipe. Be imaginative with the flavor profile and give the dish a fun or culturally relevant name. Make sure it includes creative but approachable instructions and fits within the JSON structure.`,
+        },
+      ],
+      response_format: { type: "json_object" },
+      temperature: 0.7,
+      max_tokens: 700,
+    });
 
-        const rawResponse: string | any = response.choices[0]?.message?.content;
-        const content: {title: string, ingredients: string[], instructions: string[], cookingMinutes: number, servings: number, notes: string} = JSON.parse(rawResponse);
+    const rawResponse: string | any = response.choices[0]?.message?.content;
+    const content: {
+      title: string;
+      ingredients: string[];
+      instructions: string[];
+      cookingMinutes: number;
+      servings: number;
+      notes: string;
+    } = JSON.parse(rawResponse);
 
-        if (!content) {
-            throw new Error("Received empty response.");
-        }
-
-        storeRecipe(content);
-        return content;
-    } 
-    catch (error) {
-        console.error("Error fetching recipe:", error);
-        return {
-            title: "Error",
-            ingredients: [],
-            instructions: ["Failed to generate a recipe. Please try again."],
-            cookingMinutes: "N/A",
-            servings: 0,
-            notes: "Error occurred while fetching the recipe."
-        };
+    if (!content) {
+      throw new Error("Received empty response.");
     }
+
+    storeRecipe(content, cuisine);
+    return content;
+  } catch (error) {
+    console.error("Error fetching recipe:", error);
+    return {
+      title: "Error",
+      ingredients: [],
+      instructions: ["Failed to generate a recipe. Please try again."],
+      cookingMinutes: "N/A",
+      servings: 0,
+      notes: "Error occurred while fetching the recipe.",
+    };
+  }
 };
 
-
-async function storeRecipe(recipe:{title: string, ingredients: string[], instructions: string[], cookingMinutes: number, servings: number, notes: string}) {
-    try {
-        if (!await doesRecipeExist(recipe)){
-            const newRecipe = await prisma.recipe.create({
-                data: {
-                  title: recipe.title,
-                  ingredients: recipe.ingredients,
-                  instructions: recipe.instructions,
-                  cookingMinutes: recipe.cookingMinutes,
-                  servings: recipe.servings,
-                  notes: recipe.notes
-                },
-              });
-            console.log('New user recipe:', newRecipe);
-            return newRecipe;
-        }
-    } 
-    catch (error) {
-        console.error('Error creating recipe:', error);
-        throw error;
-    } 
-    finally {
-        await prisma.$disconnect();
+async function storeRecipe(
+  recipe: {
+    title: string;
+    ingredients: string[];
+    instructions: string[];
+    cookingMinutes: number;
+    servings: number;
+    notes: string;
+  },
+  cuisine: string
+) {
+  try {
+    if (!(await doesRecipeExist(recipe))) {
+      const newRecipe = await prisma.recipe.create({
+        data: {
+          title: recipe.title,
+          ingredients: recipe.ingredients,
+          instructions: recipe.instructions,
+          cookingMinutes: recipe.cookingMinutes,
+          servings: recipe.servings,
+          cuisine: cuisine,
+          notes: recipe.notes,
+          favorite: false,
+        },
+      });
+      console.log("New user recipe:", newRecipe);
+      return newRecipe;
     }
+  } catch (error) {
+    console.error("Error creating recipe:", error);
+    throw error;
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
-async function doesRecipeExist(recipe: {title: string, ingredients: string[], instructions: string[], cookingMinutes: number, servings: number, notes: string}){
-    try{
-        const existingRecipe = await prisma.recipe.findFirst({
-            where: {
-            title: {
-                equals: recipe.title,
-                mode: 'insensitive',
-            },
-            },
-        });
-    
-        if (existingRecipe) {
-            const normalizedExistingIngredients = existingRecipe.ingredients
-            .map((ingredient: string) => ingredient.toLowerCase())
-            .sort();
-            const normalizedNewIngredients = recipe.ingredients
-            .map((ingredient) => ingredient.toLowerCase())
-            .sort();
-    
-            const normalizedExistingInstructions = existingRecipe.instructions.map((instruction: string) => instruction.toLowerCase()).join(" ");
-            const normalizedNewInstructions = recipe.instructions.map((instruction)=> instruction.toLowerCase()).join(" ");
-    
-            if ( JSON.stringify(normalizedExistingIngredients) === JSON.stringify(normalizedNewIngredients) && normalizedExistingInstructions === normalizedNewInstructions) {
-            return true;
-            }
-        }
-    
-        return false;
-    } 
-    catch (error) {
-      console.error('Error checking for duplicate recipe:', error);
-      throw error;
+async function doesRecipeExist(recipe: {
+  title: string;
+  ingredients: string[];
+  instructions: string[];
+  cookingMinutes: number;
+  servings: number;
+  notes: string;
+}) {
+  try {
+    const existingRecipe = await prisma.recipe.findFirst({
+      where: {
+        title: {
+          equals: recipe.title,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (existingRecipe) {
+      const normalizedExistingIngredients = existingRecipe.ingredients
+        .map((ingredient: string) => ingredient.toLowerCase())
+        .sort();
+      const normalizedNewIngredients = recipe.ingredients
+        .map((ingredient) => ingredient.toLowerCase())
+        .sort();
+
+      const normalizedExistingInstructions = existingRecipe.instructions
+        .map((instruction: string) => instruction.toLowerCase())
+        .join(" ");
+      const normalizedNewInstructions = recipe.instructions
+        .map((instruction) => instruction.toLowerCase())
+        .join(" ");
+
+      if (
+        JSON.stringify(normalizedExistingIngredients) ===
+          JSON.stringify(normalizedNewIngredients) &&
+        normalizedExistingInstructions === normalizedNewInstructions
+      ) {
+        return true;
+      }
     }
+
+    return false;
+  } catch (error) {
+    console.error("Error checking for duplicate recipe:", error);
+    throw error;
   }
+}
