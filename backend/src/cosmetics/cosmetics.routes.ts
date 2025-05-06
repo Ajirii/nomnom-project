9f10e71de2cd0b894a48b2d0863481617246e867
Lynn Pham <lynnpham28714@gmail.com>
2025-05-05 21:09:12 -0400

    removed extra cosmetic route for merging

--- backend/src/cosmetics/cosmetics.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getAllCosmetics } from "./cosmetics.controller";
-
-const router = Router();
-
-router.get("/", getAllCosmetics);
-
-export default router;
